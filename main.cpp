3d65f713b6000d8e4e0806fc067c361a482dd13f
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-10-18 10:59:09 +0300

    Define d and c

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+  /** Define 'x' depending on 'z' **/
   double z;
   cout << "Z: "; cin >> z;
 
@@ -17,8 +18,19 @@ int main() {
   } else {
     x = z - 1;
   }
-  
-  cout << x;
+
+  cout << x << endl;
+
+  /** Define 'd', 'c' **/
+  double d;
+  double c;
+  cout << "D: "; cin >> d;
+  cout << "C: "; cin >> c;
+
+  if (!cin.good()) { // If user entered value other than numbers
+    cout << "Error: you've entered bad values into one of the variables. All variables must be numbers. Exiting the program...";
+    return 1;
+  }
   
   return 0;
 }
