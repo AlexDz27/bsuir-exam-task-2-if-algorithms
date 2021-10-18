fd8a6dde2a82f4fc2ab6244e7fbfc1d18486f433
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-10-18 10:54:49 +0300

    Define x depending on z

--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main() {
+  double z;
+  cout << "Z: "; cin >> z;
 
+  if (!cin.good()) { // If user entered value other than numbers
+    cout << "Error: you've entered incorrect value into 'z' variable — 'z' must be a number. Exiting the program...";
+    return 1;
+  }
+
+  double x;
+  if (z < 1) {
+    x = pow(z, 2) + 1;
+  } else {
+    x = z - 1;
+  }
+  
+  cout << x;
   
   return 0;
 }
